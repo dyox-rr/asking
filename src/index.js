4776c62363832001eb3d0341a2103a4009882b0f
José Augusto <contato@joseaugustolima.com>
2021-04-30 18:24:03 -0300

    created file routes and show home page;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const routes = require('./routes');
+
+/* Configurations */
+app.use(routes);
+app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
 
 app.listen(PORT, () => console.log('App is running.'));
